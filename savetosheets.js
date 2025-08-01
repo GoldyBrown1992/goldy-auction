8be91f073c9db9fff96a38cc201bbac895818415
GoldyBrown1992 <goldysdhaliwal@gmail.com>
2025-08-01 12:08:36 -0400

    Add files via upload

---
+++ savetosheets.js
@@ -0,0 +1,19 @@
+exports.handler = async (event) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Content-Type': 'application/json'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers, body: '' };
+  }
+
+  // Just return success for now
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify({ success: true })
+  };
+};
